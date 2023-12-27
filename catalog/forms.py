9bc6c85d1b025b7d6ffbdcf5af53650f5231cff3
Linch <dr.linch19@gmail.com>
2023-12-27 20:25:33 +0300

    Правки

--- catalog/forms.py
+++ catalog/forms.py
@@ -25,6 +25,16 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
 
         return cleaned_data
 
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        prohibited_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        for obj in prohibited_list:
+            if obj in cleaned_data:
+                raise forms.ValidationError("Продукт запрещен на площадке")
+
+        return cleaned_data
+
 
 class VersionForm(forms.ModelForm):
     class Meta:
